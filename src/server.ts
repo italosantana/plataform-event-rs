482c13d50fd0ec0b5c0a4261564b6ab92fea3a64
italosantana <techitalosantana@gmail.com>
2022-08-24 07:21:58 -0300

    feat: add small fixes

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { ApolloServer } from 'apollo-server'
+import { schema } from './graphql/schema';
+
+const server = new ApolloServer({
+    schema
+})
+
+server.listen(4000, () => {
+    console.log('Server running...')
+})
