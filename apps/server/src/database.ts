f56563c96b872aaf4ab07df6190c7465152f5b66
italosantana <techitalosantana@gmail.com>
2022-10-03 17:01:24 -0300

    fix: config mongoConnect

--- apps/server/src/database.ts
+++ apps/server/src/database.ts
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 import { config } from "./config";
 
 export const connectDatabase = async (): Promise<void> => {
-    // eslint-disable-next-line no-console
-    // mongoose.connection.on('close', () => console.log('Database connection closed.'));
 
     await mongoose.connect(config.MONGO_URI);
 };
